b77c6affba24e00e98eb8c2a1d8cf4e6e1453aa7
Denis Siliukov <abhem21rus@mail.ru>
2024-01-18 07:30:07 +0300

    Project initialization

---
+++ src/main/java/com/EmployeeBook/Homework14Map/Homework14MapApplication.java
@@ -0,0 +1,13 @@
+package com.EmployeeBook.Homework14Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework14MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework14MapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmployeeBook/Homework14Map/Homework14MapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeeBook.Homework14Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework14MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
